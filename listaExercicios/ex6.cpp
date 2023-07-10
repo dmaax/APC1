f84c0718d5d84c7e0f339334b6776de62fe3e19d
lucascogrossi <lucasmachado292@gmail.com>
2023-07-10 14:50:12 -0300

    ex6

---
+++ listaExercicios/ex6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, aux;
+    cin >> a >> b >> c;
+
+    if (a > b)
+    {
+        aux = b;
+        b = a;
+        a = aux;
+    }
+    if (a > c)
+    {
+       aux = c;
+       c = a;
+       a = aux;
+    }
+    if (b > c)
+    {
+        aux = b;
+        b = c;
+        c = aux;
+    }
+  cout << a << " "<<  b << " " << c << endl;
+}
