3f633c7ff07cec7fa67098908186845435400ad8
lucascogrossi <lucasmachado292@gmail.com>
2023-07-18 16:30:55 -0300

    1051

--- Beecrowd/1051.cpp
+++ Beecrowd/1051.cpp
@@ -5,31 +5,37 @@ using namespace std;
 
 int main()
 {
-    float salary, taxes = 0;
+    float salary, taxes = 0.0;
+
     cin >> salary;
 
-    if (salary > 4500.00)
-    {
-        taxes += (salary - 4500.00) * 0.28;
-        salary -= 4500.00;
-    }
-
-    if (salary > 3000.00)
-    {
-        taxes += (salary - 3000.00) * 0.18;
-        salary -= 3000.00;
-    }
-
-    if (salary > 2000.00)
-    {
-        taxes += (salary - 2000.00) * 0.08;
-        salary -= 2000.00;
-    }
-
-    if (taxes <= 0)
+    if (salary < 2000.00)
     {
         cout << "Isento" << endl;
         return 0;
     }
-    cout << "R$ " << taxes << endl; 
-} 
+    else
+    {
+        if (salary <= 3000.00)
+        {
+            taxes += (salary - 2000.00) * 0.08;
+        }
+        else
+        {
+            taxes += 1000.00 * 0.08;
+            if (salary <= 4500.00)
+            {
+                taxes += (salary - 3000.00) * 0.18;
+            }
+            else
+            {
+                taxes += 1500.00 * 0.18;
+                if (salary > 4500.00)
+                {
+                    taxes += (salary - 4500.00) * 0.28;
+                }
+            }
+        }
+    }
+    cout << "R$ " << fixed << setprecision(2) << taxes << endl;
+}
