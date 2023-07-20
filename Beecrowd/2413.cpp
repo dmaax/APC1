be17fb9124b57231c81b188e750fa39fc7271ccf
lucascogrossi <lucasmachado292@gmail.com>
2023-07-20 10:53:12 -0300

    solved more

---
+++ Beecrowd/2413.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int firstL, secondL, thirdL;
+
+    cin >> thirdL;
+
+    secondL = thirdL * 2;
+    firstL = secondL * 2;
+
+    cout << firstL << endl;
+}
